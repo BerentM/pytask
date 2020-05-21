6dd9ea85cdabeece44f1b98b987c93a65a5b1590
Mateusz <berentm@protonmail.com>
2020-05-21 20:52:34 +0200

    task status and task update functionality, small refactoring

--- PyTask/PyTask.py
+++ PyTask/PyTask.py
@@ -12,51 +12,42 @@ PyTask is a todo.txt GUI, written in Python.
 :email: berentm@protonmail.com
 """
 
-__version__ = "0.2a"
+__version__ = "0.3a"
 
+import re
 import tkinter as tk
-from tkinter import ttk
-import PyTask.dbHelper as dbh
 from datetime import datetime
+import PyTask.dbHelper as dbh
 
 
 class PyTask:
     def __init__(self, master):
         self.master = master
+        self.db = dbh.dbHelper()
         self.focused_widget = self.master.focus_get()
         self.default_values()
         self.draw(self.master)
 
     def root_config(self, root):
         """
-        Konfiguracja głównego okna.
+        Main window configuration.
         """
         root.title("PyTask")
+        root.geometry("850x600")
 
-    def show_menu(self, event):
+    def default_values(self):
         """
-        Menu configuration.
+        Init default variables.
         """
-        self.menubar = tk.Menu(self.master)
-        filemenu = tk.Menu(self.menubar, tearoff=0)
-        filemenu.add_command(label="New", command=self.do_nothing)
-        self.menubar.add_cascade(label="File", menu=filemenu)
-        self.menubar.post(event.x_root, event.y_root)
-
-    def do_nothing(self, event):
-        """
-        Dummy function.
-        """
-        filewin = tk.Toplevel(self.master)
-        button = tk.Button(filewin, text="Do nothing button")
-        button.pack()
+        self.priority = "B"
+        self.today = datetime.strftime(datetime.today(), '%Y-%m-%d')
+        self.prev_entry = None
 
     def draw(self, master):
         """
         Draw contents of GUI.
         """
         last_row = 0
-        self.db = dbh.dbHelper()
         tk.Label(master, text="Dodaj nowe zadanie.").grid(row=last_row,
                                                           columnspan=2,
                                                           sticky='NSEW')
@@ -81,26 +72,28 @@ class PyTask:
         tk.Label(master, text='').grid(row=last_row, column=0)
 
         last_row += 1
-        tk.Label(master, text="Lista zadań").grid(row=last_row, columnspan=2,
-                                                  sticky='NSEW')
+        tk.Label(master, text=" Lista zadań").grid(row=last_row, columnspan=2,
+                                                   sticky='NSW')
 
         last_row += 1
-        self.lbox_tasks = tk.Listbox(master)
+        self.lbox_tasks = tk.Listbox(master, selectmode='Single', height=20)
         self.lbox_tasks.grid(row=last_row, columnspan=4, sticky='NSEW')
-        # self.selection = 0
-        # self.lbox_tasks.select_set(self.selection)
 
         last_row += 1
-        self.btn_add = tk.Button(master, text="Zmień", command=self.add_task)
-        self.btn_add.grid(row=last_row, column=0, sticky='NWSE')
+        self.btn_change = tk.Button(master, text="Zmień",
+                                    command=self.modify_task)
+        self.btn_change.grid(row=last_row, column=0, sticky='NWSE')
 
-        self.btn_add = tk.Button(master, text="Usuń", command=self.add_task)
-        self.btn_add.grid(row=last_row, column=1, sticky='NWSE')
+        self.btn_delete = tk.Button(master, text="Usuń",
+                                    command=self.delete_task)
+        self.btn_delete.grid(row=last_row, column=1, sticky='NWSE')
 
-        # TODO ustawic cos co bedzie zwracalo krotke z 2 parametrami
+        self.btn_switch = tk.Button(master, text="Status",
+                                    command=self.switch_status)
+        self.btn_switch.grid(row=last_row, column=2, sticky='NWS')
+
         order_tuple = (0, 0)
         self.fill_task_listbox(order_tuple)
-        self.master.focus_set()
         self.keybindings()
 
     def destroy_widgets(self):
@@ -108,18 +101,45 @@ class PyTask:
         Destroy widgets before recreating them in proper position.
         """
         self.ent_task.destroy()
-        # self.btn_add.destroy()
         self.lbox_tasks.destroy()
 
-    def add_task(self, event=None):
+    def show_menu(self, event):
         """
-        Add new task into database.
+        Menu configuration.
         """
-        if self.ent_task.get():
-            self.db.insert_task(self.ent_task.get())
-            self.destroy_widgets()
-            self.draw(self.master)
-            self.master.focus_set()
+        self.menubar = tk.Menu(self.master)
+        filemenu = tk.Menu(self.menubar, tearoff=0)
+        filemenu.add_command(label="New", command=self.do_nothing)
+        self.menubar.add_cascade(label="File", menu=filemenu)
+        self.menubar.post(event.x_root, event.y_root)
+
+    def do_nothing(self, event):
+        """
+        Dummy function.
+        """
+        filewin = tk.Toplevel(self.master)
+        button = tk.Button(filewin, text="Do nothing button")
+        button.pack()
+
+    def listbox_selection(self, event):
+        """
+        Listbox selection logic.
+        """
+        if self.lbox_tasks.curselection():
+            self.selection = self.lbox_tasks.curselection()[0]
+            self.lbox_selected = self.lbox_tasks.selection_get()
+            self.selected_task_id = re.search(r'\d*',
+                                              self.lbox_selected).group()
+
+    def format_task(self, task=[1, 'xyz']):
+        task_id, priority, date, task, task_status = task
+        if task_status == 1:
+            task_status = '✓'
+        else:
+            task_status = '_'
+        return "{:<3}|{:^3}|{:^12}| {: <50}|{:>5}".format(task_id, priority,
+                                                          date, task,
+                                                          task_status)
 
     def fill_task_listbox(self, order_tuple):
         """
@@ -130,7 +150,73 @@ class PyTask:
         task_list = self.db.get_tasks()
         task_list.sort(key=lambda tup: tup[ordered_col], reverse=asc)
         for task in task_list:
-            self.lbox_tasks.insert(tk.END, task)
+            self.lbox_tasks.insert(tk.END, self.format_task(task))
+
+    def add_task(self, event=None):
+        """
+        Add new task into database.
+        """
+        if self.ent_task.get():
+            self.db.insert_task(self.ent_priority.get(), self.ent_date.get(),
+                                self.ent_task.get())
+            self.draw(self.master)
+            self.lbox_tasks.focus_set()
+            self.lbox_tasks.activate(self.lbox_tasks.size()-1)
+            self.lbox_tasks.selection_set(self.lbox_tasks.size()-1)
+            self.lbox_tasks.see(self.lbox_tasks.size()-1)
+
+    def delete_task(self, event=None):
+        """
+        Remove task from database.
+        """
+        self.db.delete_task(self.selected_task_id)
+        self.lbox_tasks.delete(self.selection)
+
+    def modify_task(self, event=None):
+        """
+        Modify selected task.
+        """
+        def modify_database(mod_window, priority, date, task_text):
+            self.db.update_task(self.selected_task_id, priority, date,
+                                task_text)
+            self.draw(self.master)
+            mod_window.destroy()
+
+        try:
+            db_output = self.db.get_selected_task(self.selected_task_id)[0]
+
+            mod_window = tk.Toplevel(self.master)
+            lbl_priority = tk.Label(mod_window, text="Priorytet: ")
+            lbl_priority.grid(row=0, column=0, sticky="NSW")
+            ent_priority = tk.Entry(mod_window)
+            ent_priority.grid(row=0, column=1)
+            ent_priority.insert(0, db_output[0])
+
+            lbl_date = tk.Label(mod_window, text="Data: ")
+            lbl_date.grid(row=1, column=0, sticky="NSWE")
+            ent_date = tk.Entry(mod_window)
+            ent_date.grid(row=1, column=1, sticky="NSWE")
+            ent_date.insert(0, db_output[1])
+
+            lbl_task = tk.Label(mod_window, text="Zadanie: ")
+            lbl_task.grid(row=2, column=0, sticky="NSWE")
+            ent_task = tk.Entry(mod_window)
+            ent_task.grid(row=2, column=1, sticky="NSWE")
+            ent_task.insert(0, db_output[2])
+
+            btn_cancel = tk.Button(mod_window, text="Anuluj")
+            btn_cancel.grid(row=3, column=0, sticky="NSWE")
+            btn_ok = tk.Button(mod_window, text="Ok", command=lambda:
+                               modify_database(mod_window, ent_priority.get(),
+                                               ent_date.get(), ent_task.get()))
+            btn_ok.grid(row=3, column=1, sticky="NSWE")
+        except Exception:
+            pass
+
+    def switch_status(self):
+        self.db.update_task(self.selected_task_id, status=1)
+        order_tuple = (0, 0)
+        self.fill_task_listbox(order_tuple)
 
     def focus_widget(self, event, mouse=0, fnext=1):
         """
@@ -145,7 +231,6 @@ class PyTask:
             else:
                 event.widget.tk_focusPrev().focus()
         focused_widget = self.master.focus_get()
-        # print(focused_widget)
         self.modify_focused_widget(focused_widget)
 
     def modify_focused_widget(self, focused_widget):
@@ -170,10 +255,6 @@ class PyTask:
             self.prev_entry = focused_widget
             pass
         elif str_focused_widget == '.!listbox':
-            # keyboard support is based on function entry_selection
-            # mouse support
-            # self.lbox_selected = self.lbox_tasks.get(tk.ANCHOR)
-            # print(self.lbox_selected)
             pass
 
     def keybindings(self):
@@ -189,54 +270,14 @@ class PyTask:
         self.master.bind("<a>", lambda a: self.ent_task.focus_set())
         self.ent_task.bind("<Return>", self.add_task)
         self.btn_add.bind("<Return>", self.add_task)
-        self.lbox_tasks.bind("<<ListboxSelect>>", self.entry_selection)
-
-    def default_values(self):
-        """
-        Init default variables.
-        """
-        self.priority = "B"
-        self.today = datetime.strftime(datetime.today(), '%Y-%m-%d')
-        self.prev_entry = None
-
-    def entry_selection(self, event):
-        """
-        Listbox selection logic.
-        """
-        if self.lbox_tasks.curselection():
-            self.selection = self.lbox_tasks.curselection()[0]
-        else:
-            self.selection = 0
-        self.lbox_tasks.select_set(self.selection)
-        self.lbox_selected = self.lbox_tasks.selection_get()
-        print(self.selection)
-        print(self.lbox_selected)
-
-
-class ScrollableFrame(ttk.Frame):
-    def __init__(self, container, *args, **kwargs):
-        super().__init__(container, *args, **kwargs)
-        canvas = tk.Canvas(self, height=600, width=800)
-        scrollbar = ttk.Scrollbar(self, orient="vertical",
-                                  command=canvas.yview)
-        self.scrollable_frame = ttk.Frame(canvas)
-
-        self.scrollable_frame.bind("<Configure>", lambda e: canvas.configure(
-            scrollregion=canvas.bbox("all")))
-        self.scrollable_frame.focus_set()
-
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        scrollbar.pack(side="right", fill="y")
-        canvas.pack(side="left", fill="both", expand=True)
+        self.btn_delete.bind("<Return>", self.delete_task)
+        self.lbox_tasks.bind("<d>", self.delete_task)
+        self.lbox_tasks.bind("<Delete>", self.delete_task)
+        self.lbox_tasks.bind("<<ListboxSelect>>", self.listbox_selection)
+        self.lbox_tasks.bind("<Return>", self.modify_task)
 
 
 root = tk.Tk()
-# frame = ScrollableFrame(root)
-# frame.pack()
-# pytask = PyTask(frame.scrollable_frame)
 pytask = PyTask(root)
 
 pytask.root_config(root)
--- PyTask/dbHelper.py
+++ PyTask/dbHelper.py
@@ -19,7 +19,8 @@ class dbHelper:
         Create new database/tables if they don't exist.
         """
         self.cur.execute("CREATE TABLE IF NOT EXISTS tasks (id INTEGER\
-                         PRIMARY KEY, task TEXT)")
+                         PRIMARY KEY, priority TEXT, date DATE, task TEXT,\
+                         task_status INTEGER DEFAULT 0)")
 
     def purge_database(self):
         """
@@ -30,25 +31,47 @@ class dbHelper:
         for table_name in tables:
             self.cur.execute(f"DROP TABLE {table_name[0]}")
 
-    def insert_task(self, task_text):
+    def insert_task(self, priority, date, task_text):
         """
         Insert new task into database.
         ~~~~~~~~~~~~~~~~~~~~~~~~
 
         :task_text: short task description
         """
-        self.cur.execute(f"INSERT INTO tasks(task) VALUES ('{task_text}')")
+        self.cur.execute(f"INSERT INTO tasks(priority, date, task) VALUES\
+                         ('{priority}', '{date}', '{task_text}')")
+        self.conn.commit()
+
+    def delete_task(self, task_id):
+        self.cur.execute(f"DELETE FROM tasks WHERE id ={task_id}")
         self.conn.commit()
 
     def get_tasks(self):
         """
         Get list of tasks.
         """
-        output = self.cur.execute("SELECT id, task FROM tasks")
+        output = self.cur.execute("SELECT id, priority, date, task, task_status FROM tasks")
         if output:
             return output.fetchall()
         else:
             return []
+
+    def get_selected_task(self, task_id):
+        output = self.cur.execute(f"SELECT priority, date, task FROM tasks\
+                                  WHERE id = {task_id}")
+        if output:
+            return output.fetchall()
+        else:
+            return []
+
+    def update_task(self, task_id, priority=None, date=None, task_text=None, status=None):
+        if priority:
+            self.cur.execute(f"UPDATE tasks SET priority = '{priority}', date =\
+                            '{date}', task = '{task_text}' WHERE id = {task_id}")
+        elif status:
+            self.cur.execute(f"UPDATE tasks SET task_status = CASE WHEN task_status = 0\
+                             THEN 1 ELSE 0 END WHERE id = {task_id}")
+        self.conn.commit()
 
 
 if __name__ == "__main__":
